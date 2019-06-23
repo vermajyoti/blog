38307c1d9e9b389f38adc01cdc12426397780a6f
verma jyoti <vj.d2096@gmail.com>
2019-06-23 12:22:06 +0530

    final commit

--- index.js
+++ index.js
@@ -1,42 +1,67 @@
-const path = require('path');
-const expressEdge = require('express-edge');
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const Post= require('./database/models/Post');
+const logoutController = require("./controllers/logout");
+const edge = require("edge.js");	
+const connectFlash = require("connect-flash");
+const auth = require("./middleware/auth");
+const expressEdge = require("express-edge");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+const expressSession = require('express-session');
+const connectMongo = require('connect-mongo');
+const storePost = require('./middleware/storePost'); 
+const createPostController = require('./controllers/createPost');
+const homePageController = require('./controllers/homePage');
+const storePostController = require('./controllers/storePost');
+const getPostController = require('./controllers/getPost');
+const createUserController = require("./controllers/createUser");
+const storeUserController = require('./controllers/storeUser');
+const loginController = require("./controllers/login");
+const loginUserController = require('./controllers/loginUser');
+const redirectIfAuthenticated = require('./middleware/redirectIfAuthenticated');
  
 const app = new express();
- 
+app.use(connectFlash());
+
 mongoose.connect('mongodb://localhost:27017/node-blog', { useNewUrlParser: true })
- .then(() => 'You are now connected to Mongo!')
- .catch(err => console.error('Something went wrong', err))
-
-app.use(express.static('public'));
-app.use(expressEdge);
-app.set('views',__dirname+'/views');
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({
-    extended:true
-
+    .then(() => 'You are now connected to Mongo!')
+    .catch(err => console.error('Something went wrong', err))
+ 
+    const mongoStore = connectMongo(expressSession);
+ 
+app.use(expressSession({
+    secret: 'secret',
+    store: new mongoStore({
+        mongooseConnection: mongoose.connection
+    })
 }));
-app.get('/',(req,res)=>{
- res.render('index');
-});
-app.get('/posts/new',(req,res)=>{
-    res.render('create');
+app.use(fileUpload());
+app.use(express.static("public"));
+app.use(expressEdge);
+app.set('views', __dirname + '/views');
+ 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+ 
+ 
+app.use('/posts/store', storePost)
+app.use('*', (req, res, next) => {
+  edge.global('auth', req.session.userId)
+  next()
 });
 
-app.post('/posts/store', (req, res) => {
-    Post.create(req.body, (error, post) => {
-        res.redirect('/')
-    })
-});
-app.get('/', async (req, res) => {
-    const posts = await Post.find({})
-    res.render('index', {
-        posts
-    })
-});
-app.listen(8080,()=>{
-    console.log('app listing on port 8080')
+app.get("/auth/logout", logoutController);
+
+
+app.get("/", homePageController);
+app.get("/post/:id", getPostController);
+app.get("/posts/new", auth, createPostController);
+app.post("/posts/store", storePostController);
+app.get("/auth/login", redirectIfAuthenticated, loginController);
+app.post("/users/login", redirectIfAuthenticated, loginUserController);
+app.get("/auth/register", redirectIfAuthenticated, createUserController);
+app.post("/users/register", redirectIfAuthenticated, storeUserController);
+
+app.listen(8080, () => {
+  console.log("App listening on port 8080");
 });
